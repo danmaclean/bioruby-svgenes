d38bd39a869bb81d162842f6645c6acd2fcc68c0
Dan MacLean <maclean.daniel@gmail.com>
2012-09-28 16:12:06 +0100

    Initial commit to bio-svgenes.

---
+++ lib/bio-svgenes.rb
@@ -0,0 +1,11 @@
+# Please require your code below, respecting the bioruby directory tree. 
+# For instance, perhaps the only uncommented line in this file might 
+# be something like this:
+#
+#   require 'bio/sequence/awesome_sequence_plugin_thingy'
+#
+# and then create the ruby file 'lib/bio/sequence/awesome_sequence_thingy.rb'
+# and put your plugin's code there. It is bad practice to write other code 
+# directly into this file, because doing so causes confusion if this biogem 
+# was ever to get merged into the main bioruby tree.
+
